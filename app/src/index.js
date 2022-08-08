9241bb91700f0a039095ccaa4226de298737d69c
Beneboehlke <69843173+Beneboehlke@users.noreply.github.com>
2022-08-08 19:31:44 +0200

    test functionaliy

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 function init() {
-    console.log("### Starting MME Project ###"); // eslint-disable-line no-console
+  console.log("### Starting MME Project ###"); // eslint-disable-line no-console
+  console.log("this is a functionality test");
 }
 
 init();
