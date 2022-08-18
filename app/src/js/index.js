444ad50a3abde359ae0a0d2b3da9dfda5d4923b3
Beneboehlke <69843173+Beneboehlke@users.noreply.github.com>
2022-08-18 13:49:06 +0200

    FEAT | basic layout main

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,3 +1,20 @@
+const hamburger = document.querySelector("#burger-menu");
+const closeIcon = document.querySelector("#x-burger-menu");
+const profile = document.querySelector(".profile-container");
+
+function toggleMenu() {
+  if (profile.classList.contains("showMenu")) {
+    profile.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+  } else {
+    profile.classList.add("showMenu");
+    closeIcon.style.display = "block";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+closeIcon.addEventListener("click", toggleMenu);
+
 function init() {
   console.log("### Starting MME Project ###"); // eslint-disable-line no-console
 }
