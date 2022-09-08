9ef5d0b067c9139b0979f0ecdf1af57850468217
NaCesca <69862866+NaCesca@users.noreply.github.com>
2022-09-08 15:00:57 +0200

    updated files structure

--- app/src/js/entries.js
+++ app/src/js/entries.js
@@ -1,13 +1,36 @@
-import Manager from "./Manager.js";
+import Entry from "resources/js/entries.js";
+import Observable from "./utils/Observable";
 
 
-class Entry {
-
-    constructor(name, category, )
+function initEntry(){
+    let entryEl = document.querySelector("#entry-popup");
+    entry = new Entry(entryEl);
+    initInputs(entry);
+    initEvents(entry);
 }
-function init() {
-    let entryArray = [];
+function initInputs(entry){
+entry.inputs = {
+    //transport
+    carEl : entry.el.querySelector("#car-km-field"),
+    busEl : entry.el.querySelector("#bus-km-field"),
+    longTransEl : entry.el.querySelector("#long-distance-transport-km-field"),
+    publicTransEl : entry.el.querySelector("#public-transport-km-field"),
+    noneTransEl : entry.el.querySelector("#default-km-field"),
+
+    //food
+
+
+    //other
+    
+};
+}
 
 
 
+class EntryView extends Observable{
+    constructor(entry){
+        this.entry = entry;
+
+    }
+    
 }
