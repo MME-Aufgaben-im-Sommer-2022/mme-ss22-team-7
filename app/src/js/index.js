3f5fbbe19e7025a8683b8c0a08efa4f049daa1a0
FeistFlo <florian.feist.99@gmail.com>
2022-09-12 17:10:57 +0200

    Save-Button-PopUp-Closing implemented
    Co-authored-by: Johannes Klein <johnnyklein@users.noreply.github.com>
    Co-authored-by: NaCesca <NaCesca@users.noreply.github.com>

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,28 +1,11 @@
 import {initEntryPage} from "./entries/entries.js";
 
-// const hamburger = document.querySelector("#burger-menu"),
-//   closeIcon = document.querySelector("#x-burger-menu"),
-//   profile = document.querySelector(".profile-container"),
 
 console.log("start");
-
 const entryButton = document.querySelector(".new-entry-button"),
-  entriesPopUp = document.querySelector(".overlay-entries");
-
-//hamburger.addEventListener("click", toggleMenu);
-//closeIcon.addEventListener("click", toggleMenu);
-
+entriesPopUp = document.querySelector(".overlay-entries");
+//new entry Button
 entryButton.addEventListener("click", toggleEntries);
-
-// function toggleMenu() {
-//   if (profile.classList.contains("showMenu")) {
-//     profile.classList.remove("showMenu");
-//     closeIcon.style.display = "none";
-//   } else {
-//     profile.classList.add("showMenu");
-//     closeIcon.style.display = "block";
-//   }
-// }
 
 function toggleEntries() {
   if (entriesPopUp.classList.contains("showMenu")) {
@@ -31,6 +14,10 @@ function toggleEntries() {
   } else {
     entriesPopUp.classList.add("showMenu");
     entriesPopUp.style.display = "block";
+    let iframe = document.querySelector(".popUp"),
+    safeButtonEl = iframe.contentWindow.document.querySelector("#save-button");
+    //saveButton
+    safeButtonEl.addEventListener("click", onEntrySave);
     initEntryPage();
   }
 }
@@ -44,7 +31,33 @@ function init() {
   console.log("### Starting MME Project ###"); // eslint-disable-line no-console
   //ausführen vom weiteren Code funktioniert manchmal nicht wegen "favico.icon" Fehler
   //EntryView.init();
-  entriesPopUp.addEventListener("saved", onEntrySave);
+  //entriesPopUp.addEventListener("saved", onEntrySave);
 }
 
 init();
+
+
+
+
+
+
+
+// const hamburger = document.querySelector("#burger-menu"),
+//   closeIcon = document.querySelector("#x-burger-menu"),
+//   profile = document.querySelector(".profile-container"),
+
+
+//hamburger.addEventListener("click", toggleMenu);
+//closeIcon.addEventListener("click", toggleMenu);
+
+
+
+// function toggleMenu() {
+//   if (profile.classList.contains("showMenu")) {
+//     profile.classList.remove("showMenu");
+//     closeIcon.style.display = "none";
+//   } else {
+//     profile.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//   }
+// }
