e89d598ed1b4209455a47381d1eb5db5a65d1282
FeistFlo <florian.feist.99@gmail.com>
2022-09-12 17:11:56 +0200

    Save-Button-PopUp-Closing implemented

--- app/src/js/entries/entries.js
+++ app/src/js/entries/entries.js
@@ -1,4 +1,3 @@
-import Observable from "../utils/Observable.js";
 import {initInputs} from "./entryData.js";
 
 console.log("EntryPage loaded");
@@ -6,41 +5,8 @@ initEntryPage();
 
 function initEntryPage(){
     if(document.querySelector("#car-km-field") != null){
-    //let entryEl = document.querySelector("#entry-popup");
-    console.log("initInputs");
     let inputs = initInputs();
-    console.log("saveBtn");
-    saveBtn();
     }
 }
 
-
-function saveBtn(){
-    console.log("methode savebtn");
-
-    let saveBtnEl = document.querySelector("#save-button");
-    saveBtnEl.addEventListener("click", onSaveBtnClick);
-}
-
-function onSaveBtnClick(){
-    console.log("methode onSaveClick");
-
-    console.log("Saved -> null");
-    let event = new Event("saved", null);
-
-    console.log("notifyAll funktioniert!!");
-    view.notifyAll(event);
-}
-
-class EntryView extends Observable{
-
-    init(){
-        console.log("init");
-    }
-}
-
-let view = new EntryView();
-console.log("new EntryView erstellt");
-
-export default EntryView;
 export {initEntryPage};
