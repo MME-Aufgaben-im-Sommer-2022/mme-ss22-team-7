907792b5edde9faeaf8820698bdf57f9d28da61b
FeistFlo <florian.feist.99@gmail.com>
2022-09-15 14:48:53 +0200

    buxfixing

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -20,7 +20,6 @@ entryButton.addEventListener("click", toggleEntries);
 hamburger.addEventListener("click", toggleMenu);
 closeIcon.addEventListener("click", toggleMenu);
 
-entryButton.addEventListener("click", toggleEntries);
 let login = "none";
 handleLoginPopup();
 
@@ -133,8 +132,6 @@ account.create("unique()", "me@example.com", "password", "Jane Doe").then(
 //   closeIcon = document.querySelector("#x-burger-menu"),
 //   profile = document.querySelector(".profile-container"),
 
-const entryButton = document.querySelector(".new-entry-button"),
-  entriesPopUp = document.querySelector(".overlay-entries");
 
 //hamburger.addEventListener("click", toggleMenu);
 //closeIcon.addEventListener("click", toggleMenu);
@@ -150,19 +147,3 @@ const entryButton = document.querySelector(".new-entry-button"),
 //     closeIcon.style.display = "block";
 //   }
 // }
-
-function toggleEntries() {
-  if (entriesPopUp.classList.contains("showMenu")) {
-    entriesPopUp.classList.remove("showMenu");
-    entriesPopUp.style.display = "none";
-  } else {
-    entriesPopUp.classList.add("showMenu");
-    entriesPopUp.style.display = "block";
-  }
-}
-
-function init() {
-  console.log("### Starting MME Project ###"); // eslint-disable-line no-console
-}
-
-init();
