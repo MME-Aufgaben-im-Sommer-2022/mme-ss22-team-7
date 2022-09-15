db9ef7bdcf02c8645ee621bd5974404b54764192
Beneboehlke <69843173+Beneboehlke@users.noreply.github.com>
2022-09-15 13:17:32 +0200

    WIP | database - trying to get initial settings to work - cdn import may not be functioning

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,11 +1,32 @@
+// Init your Web SDK
+const client = new Client();
+
+client
+  .setEndpoint("http://localhost/v1") // Your Appwrite Endpoint
+  .setProject("455x34dfkj"); // Your project ID
+
+const account = new Account(client);
+
+// Register User
+account.create("unique()", "me@example.com", "password", "Jane Doe").then(
+  (response) => {
+    console.log(response);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+/**
+ * für das database / appwrite Problem irrelevanter Code
+ */
+
 // const hamburger = document.querySelector("#burger-menu"),
 //   closeIcon = document.querySelector("#x-burger-menu"),
 //   profile = document.querySelector(".profile-container"),
 
-
 const entryButton = document.querySelector(".new-entry-button"),
   entriesPopUp = document.querySelector(".overlay-entries");
-
 
 //hamburger.addEventListener("click", toggleMenu);
 //closeIcon.addEventListener("click", toggleMenu);
@@ -30,7 +51,7 @@ function toggleEntries() {
     entriesPopUp.style.display = "block";
   }
 }
- 
+
 function init() {
   console.log("### Starting MME Project ###"); // eslint-disable-line no-console
 }
